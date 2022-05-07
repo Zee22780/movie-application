f047d0d251073c692c3fbc1cbc2f841a86f4b307
zuri227 <zuri22780@gmail.com>
2022-05-06 23:37:21 -0400

    Add basic styling to App.css

--- src/App.js
+++ src/App.js
@@ -2,35 +2,20 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 
 function App() {
 
-  const [movies, setMovies] = useState([
-  {
-      "Title": "Kill Bill: Vol. 1",
-      "Year": "2003",
-      "imdbID": "tt0266697",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNzM3NDFhYTAtYmU5Mi00NGRmLTljYjgtMDkyODQ4MjNkMGY2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-  },
-  {
-      "Title": "Kill Bill: Vol. 2",
-      "Year": "2004",
-      "imdbID": "tt0378194",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNmFiYmJmN2QtNWQwMi00MzliLThiOWMtZjQxNGRhZTQ1MjgyXkEyXkFqcGdeQXVyNzQ1ODk3MTQ@._V1_SX300.jpg"
-  },
-  {
-      "Title": "Kill Bill: The Whole Bloody Affair",
-      "Year": "2011",
-      "imdbID": "tt6019206",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNzQyZTgyYmQtNmExMS00YzNkLWIyOTAtYmVkNjAwZGE5ZDM1XkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"
-  },
-])
+  const [movies, setMovies] = useState([])
+  const [searchValue, setSearchValue] = useState('')
 
   return (
     <div className="container-fluid movie-app">
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+      </div>
       <div className="row">
         <MovieList 
           movies={movies}
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,15 @@
+
+const SearchBox = (props) => {
+  return ( 
+    <div className="col col-sm-4">
+      <input 
+        className="form-control"
+        value={props.value}
+        onChange={(event) => props.setSearchValue(event.target.value)}
+        placeholder="Type to search..."
+      />
+    </div>
+   );
+}
+
+export default SearchBox;
