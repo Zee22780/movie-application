25b84a19501aa36dff25cb9bf99d218aacdc004c
zuri227 <zuri22780@gmail.com>
2022-05-07 00:50:34 -0400

    Add FavoriteComponent and onclick code to MovieList component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,17 @@
-
 const MovieList = (props) => {
-  const FavoriteComponent = props.FavoriteComponent
+  const FavoriteComponent = props.favoriteComponent
 
   return ( 
     <>
       {props.movies.map( (movie, index) => (
         <div className="image-container d-flex justify-content-start m-3">
           <img src={movie.Poster} alt="movie" />
+          <div
+            onClick = {() => props.handleFavoritesClick(movie)}
+            className="overlay d-flex align-items-center justify-content-center"
+            >
+            <FavoriteComponent />
+          </div>
         </div>
       ))}
     </>
